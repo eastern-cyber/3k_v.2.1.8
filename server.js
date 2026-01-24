e9eb5583cbc5fc332e052f805a8ff5b02c0cc367
katawutp <katawut.paramee@gmail.com.com>
2026-01-25 01:31:42 +0700

    server.js 3kv218

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // ========== MIDDLEWARE ==========
 app.use(cors({
-    origin: ['https://3kv215.dfi.fund', 'http://localhost:3000'],
+    origin: ['https://3kv218.dfi.fund', 'http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
